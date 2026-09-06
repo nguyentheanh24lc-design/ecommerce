3b4a9c1542b08818ad9b161443c9f3d8f9072945
nguyentheanh24lc-design <nguyentheanh24lc@gmail.com>
2026-09-06 11:07:58 +0700

    Add InvalidQuantityException custom exception

---
+++ src/exception/InvalidQuantityException.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class InvalidQuantityException extends Exception {
+
+    public InvalidQuantityException(String message) {
+        super(message);
+    }
+
+    public InvalidQuantityException() {
+        this("Product quantity must be greater than 0.");
+    }
+}
--- src/model/OrderItem.java
+++ src/model/OrderItem.java
@@ -1,121 +1,137 @@
 package model;
 
+import exception.InvalidQuantityException;
+
 import java.util.Objects;
 
 public class OrderItem {
 
-private Product product;
-private int quantity;
+    private Product product;
+    private int quantity;
 
-public OrderItem() {
-    this(
-            new Product(),
-            1
-    );
-}
+    public OrderItem() {
+        this(new Product(), 1);
+    }
 
-public OrderItem(Product product, int quantity) {
-    setProduct(product);
-    setQuantity(quantity);
-}
+    public OrderItem(Product product, int quantity) {
+        setProduct(product);
 
-// Getter & Setter
+        try {
+            setQuantity(quantity);
+        } catch (InvalidQuantityException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
 
-public Product getProduct() {
-    return product;
-}
+    // =========================
+    // PRODUCT
+    // =========================
 
-public void setProduct(Product product) {
+    public Product getProduct() {
+        return product;
+    }
 
-    if (product == null) {
-        throw new IllegalArgumentException(
-                "Product cannot be null."
+    public void setProduct(Product product) {
+        if (product == null) {
+            throw new IllegalArgumentException(
+                    "Product cannot be null."
+            );
+        }
+
+        this.product = product;
+    }
+
+    // =========================
+    // QUANTITY
+    // =========================
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity)
+            throws InvalidQuantityException {
+
+        if (quantity <= 0) {
+            throw new InvalidQuantityException(
+                    "Product quantity must be greater than 0."
+            );
+        }
+
+        this.quantity = quantity;
+    }
+
+    // =========================
+    // BUSINESS METHODS
+    // =========================
+
+    public double calculateSubtotal() {
+        return product.getPrice() * quantity;
+    }
+
+    public void increaseQuantity(int amount) {
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException(
+                    "Increase amount must be greater than 0."
+            );
+        }
+
+        quantity += amount;
+    }
+
+    public void decreaseQuantity(int amount) {
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException(
+                    "Decrease amount must be greater than 0."
+            );
+        }
+
+        if (quantity - amount <= 0) {
+            throw new IllegalArgumentException(
+                    "Quantity must be greater than 0."
+            );
+        }
+
+        quantity -= amount;
+    }
+
+    // =========================
+    // OBJECT METHODS
+    // =========================
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "productId='" + product.getId() + '\'' +
+                ", productName='" + product.getName() + '\'' +
+                ", quantity=" + quantity +
+                ", subtotal=" + calculateSubtotal() +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof OrderItem)) {
+            return false;
+        }
+
+        OrderItem orderItem = (OrderItem) obj;
+
+        return Objects.equals(
+                product.getId(),
+                orderItem.product.getId()
         );
     }
 
-    this.product = product;
-}
-
-public int getQuantity() {
-    return quantity;
-}
-
-public void setQuantity(int quantity) {
-
-    if (quantity <= 0) {
-        throw new IllegalArgumentException(
-                "Quantity must be greater than 0."
-        );
+    @Override
+    public int hashCode() {
+        return Objects.hash(product.getId());
     }
-
-    this.quantity = quantity;
-}
-
-public double calculateSubtotal() {
-
-    return product.getPrice() * quantity;
-}
-
-public void increaseQuantity(int amount) {
-
-    if (amount <= 0) {
-        throw new IllegalArgumentException(
-                "Increase amount must be greater than 0."
-        );
-    }
-
-    quantity += amount;
-}
-
-public void decreaseQuantity(int amount) {
-
-    if (amount <= 0) {
-        throw new IllegalArgumentException(
-                "Decrease amount must be greater than 0."
-        );
-    }
-
-    if (quantity - amount <= 0) {
-        throw new IllegalArgumentException(
-                "Quantity must be greater than 0."
-        );
-    }
-
-    quantity -= amount;
-}
-
-@Override
-public String toString() {
-
-    return "OrderItem{" +
-            "productId='" + product.getId() + '\'' +
-            ", productName='" + product.getName() + '\'' +
-            ", quantity=" + quantity +
-            ", subtotal=" + calculateSubtotal() +
-            '}';
-}
-
-@Override
-public boolean equals(Object obj) {
-
-    if (this == obj) {
-        return true;
-    }
-
-    if (!(obj instanceof OrderItem)) {
-        return false;
-    }
-
-    OrderItem orderItem = (OrderItem) obj;
-
-    return Objects.equals(
-            product.getId(),
-            orderItem.product.getId()
-    );
-}
-
-@Override
-public int hashCode() {
-    return Objects.hash(product.getId());
-}
 }
